7acdd63d1046688905619c52167de8f1ecf26245
xzfcxdy123 <2984778592@qq.com>
2024-01-25 15:03:09 +0800

    add: initialization project openapi

---
+++ src/test/java/love/xlxz/openapi/OpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package love.xlxz.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
